4d427110c661209cf11ecc0eb20f5eead7cca3e7
Siddhant03092001 <122206666+Siddhant03092001@users.noreply.github.com>
2023-03-23 18:00:02 +0530

    Delete new project directory

--- new project/script/resp.js
+++
@@ -1,9 +0,0 @@
-burger= document.querySelector('.burger')
-navbar= document.querySelector('.navbar')
-navList= document.querySelector('.nav-list')
-
-burger.addEventListener('click',()=>{
-    navbar.classList.toggle('h-nav-resp')
-    navList.classList.toggle('v-class-resp')
-    
-})
